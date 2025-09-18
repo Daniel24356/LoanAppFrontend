bc09e1b73c478a209a777bdc784cbe4ead5e6368
Daniel24356 <igwedaniel128@gmail.com>
2025-09-18 14:54:13 -0700

    uncommented card.jsx

--- src/middleComponent/Card.jsx
+++ src/middleComponent/Card.jsx
@@ -1,39 +1,39 @@
 
 
-// const Card = () => {
-//     return(
-//         <>
-//         <div>
-//             <div>
-//                 <div>
-//                     <p>Account Number</p>
-//                     <p>702 938 2932</p>
-//                 </div>
-//                 <div>
-//                     <img src="" alt="" />
-//                 </div>
-//             </div>
-//             <div>
-//                 <p>1929 **** **** 3332</p>
-//             </div>
-//             <div>
-//                 <div>
-//                    <p>VALID</p>
-//                    <p>THRU</p>
-//                    <p>08/21</p>
-//                 </div>
-//                 <div>
-//                     <p>CARD HOLDER</p>
-//                     <p>BURNA BOY</p>
-//                 </div>
-//             </div>
-//         </div>
-//         <div>
-//             {/* icons */}
-//             {/* icons */}
-//         </div>
-//         </>
-//     )
-// }
+const Card = () => {
+    return(
+        <>
+        <div>
+            <div>
+                <div>
+                    <p>Account Number</p>
+                    <p>702 938 2932</p>
+                </div>
+                <div>
+                    <img src="" alt="" />
+                </div>
+            </div>
+            <div>
+                <p>1929 **** **** 3332</p>
+            </div>
+            <div>
+                <div>
+                   <p>VALID</p>
+                   <p>THRU</p>
+                   <p>08/21</p>
+                </div>
+                <div>
+                    <p>CARD HOLDER</p>
+                    <p>BURNA BOY</p>
+                </div>
+            </div>
+        </div>
+        <div>
+            {/* icons */}
+            {/* icons */}
+        </div>
+        </>
+    )
+}
 
-// export default Card
+export default Card
