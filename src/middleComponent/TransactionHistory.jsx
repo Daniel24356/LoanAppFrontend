52292de0576da93cd7afff74284a8ee3f2d91dda
Daniel24356 <igwedaniel128@gmail.com>
2025-09-18 14:58:31 -0700

    commented TransactionHistory.jsx

--- src/middleComponent/TransactionHistory.jsx
+++ src/middleComponent/TransactionHistory.jsx
@@ -1,101 +1,101 @@
 
 
-const TransactionHistory = () => {
-    return(
-        <>
-          <div>
-            <div>
-                <p>Transaction History</p>
-                <div>
-                    <div>Monthly</div>
-                    <div>Weekly</div>
-                    <div>Today</div>
-                </div>
-            </div>
-            <div>
-                <p>Name</p>
-                <p>Date/time</p>
-                <p>Amount/Card No</p>
-                <p>Status</p>
-            </div>
-            <div>
-                <div>
-                    <p>Links Bank</p>
-                    <p> Ref# 1203948384</p>
-                </div>
-                <div>
-                    <p>June 4, 2020</p>
-                    <p>05:34:45 AM</p>
-                </div>
-                <div>
-                    <p>$5,553,000,000.00</p>
-                    <p>****4593</p>
-                </div>
-                <button>Successful</button>
-            </div>
-            <div>
-                <div>
-                    <p>Links Bank</p>
-                    <p> Ref# 1203948384</p>
-                </div>
-                <div>
-                    <p>June 4, 2020</p>
-                    <p>05:34:45 AM</p>
-                </div>
-                <div>
-                    <p>$5,553,000,000.00</p>
-                    <p>****4593</p>
-                </div>
-                <button>Successful</button>
-            </div>
-            <div>
-                <div>
-                    <p>Links Bank</p>
-                    <p> Ref# 1203948384</p>
-                </div>
-                <div>
-                    <p>June 4, 2020</p>
-                    <p>05:34:45 AM</p>
-                </div>
-                <div>
-                    <p>$5,553,000,000.00</p>
-                    <p>****4593</p>
-                </div>
-                <button>Successful</button>
-            </div>
-            <div>
-                <div>
-                    <p>Links Bank</p>
-                    <p> Ref# 1203948384</p>
-                </div>
-                <div>
-                    <p>June 4, 2020</p>
-                    <p>05:34:45 AM</p>
-                </div>
-                <div>
-                    <p>$5,553,000,000.00</p>
-                    <p>****4593</p>
-                </div>
-                <button>Successful</button>
-            </div>
-            <div>
-                <div>
-                    <p>Links Bank</p>
-                    <p> Ref# 1203948384</p>
-                </div>
-                <div>
-                    <p>June 4, 2020</p>
-                    <p>05:34:45 AM</p>
-                </div>
-                <div>
-                    <p>$5,553,000,000.00</p>
-                    <p>****4593</p>
-                </div>
-                <button>Successful</button>
-            </div>
-          </div>
-        </>
-    )
-}
+// const TransactionHistory = () => {
+//     return(
+//         <>
+//           <div>
+//             <div>
+//                 <p>Transaction History</p>
+//                 <div>
+//                     <div>Monthly</div>
+//                     <div>Weekly</div>
+//                     <div>Today</div>
+//                 </div>
+//             </div>
+//             <div>
+//                 <p>Name</p>
+//                 <p>Date/time</p>
+//                 <p>Amount/Card No</p>
+//                 <p>Status</p>
+//             </div>
+//             <div>
+//                 <div>
+//                     <p>Links Bank</p>
+//                     <p> Ref# 1203948384</p>
+//                 </div>
+//                 <div>
+//                     <p>June 4, 2020</p>
+//                     <p>05:34:45 AM</p>
+//                 </div>
+//                 <div>
+//                     <p>$5,553,000,000.00</p>
+//                     <p>****4593</p>
+//                 </div>
+//                 <button>Successful</button>
+//             </div>
+//             <div>
+//                 <div>
+//                     <p>Links Bank</p>
+//                     <p> Ref# 1203948384</p>
+//                 </div>
+//                 <div>
+//                     <p>June 4, 2020</p>
+//                     <p>05:34:45 AM</p>
+//                 </div>
+//                 <div>
+//                     <p>$5,553,000,000.00</p>
+//                     <p>****4593</p>
+//                 </div>
+//                 <button>Successful</button>
+//             </div>
+//             <div>
+//                 <div>
+//                     <p>Links Bank</p>
+//                     <p> Ref# 1203948384</p>
+//                 </div>
+//                 <div>
+//                     <p>June 4, 2020</p>
+//                     <p>05:34:45 AM</p>
+//                 </div>
+//                 <div>
+//                     <p>$5,553,000,000.00</p>
+//                     <p>****4593</p>
+//                 </div>
+//                 <button>Successful</button>
+//             </div>
+//             <div>
+//                 <div>
+//                     <p>Links Bank</p>
+//                     <p> Ref# 1203948384</p>
+//                 </div>
+//                 <div>
+//                     <p>June 4, 2020</p>
+//                     <p>05:34:45 AM</p>
+//                 </div>
+//                 <div>
+//                     <p>$5,553,000,000.00</p>
+//                     <p>****4593</p>
+//                 </div>
+//                 <button>Successful</button>
+//             </div>
+//             <div>
+//                 <div>
+//                     <p>Links Bank</p>
+//                     <p> Ref# 1203948384</p>
+//                 </div>
+//                 <div>
+//                     <p>June 4, 2020</p>
+//                     <p>05:34:45 AM</p>
+//                 </div>
+//                 <div>
+//                     <p>$5,553,000,000.00</p>
+//                     <p>****4593</p>
+//                 </div>
+//                 <button>Successful</button>
+//             </div>
+//           </div>
+//         </>
+//     )
+// }
 
-export default TransactionHistory
+// export default TransactionHistory
