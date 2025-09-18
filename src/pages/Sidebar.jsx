3c80acf6a3d966414af27133a1a3f42eae006f60
Daniel24356 <igwedaniel128@gmail.com>
2025-09-18 15:09:19 -0700

    commented sidebar.jsx

--- src/pages/Sidebar.jsx
+++ src/pages/Sidebar.jsx
@@ -1,31 +1,31 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import "./Sidebar.css"
-// import { IoHomeSharp } from "react-icons/io5";
-// import { FaSackDollar } from "react-icons/fa6";
-// import { FiRefreshCcw } from "react-icons/fi"; 
+// import React, { useState } from "react";
+// import { Link } from "react-router-dom";
+// import "./Sidebar.css"
+// // import { IoHomeSharp } from "react-icons/io5";
+// // import { FaSackDollar } from "react-icons/fa6";
+// // import { FiRefreshCcw } from "react-icons/fi"; 
 
-const Sidebar = () => {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+// const Sidebar = () => {
+//   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
 
-  return (
-      <aside className={`sidebar ${isSidebarOpen ? "open" : "closed"}`}>
-        <button className="toggle-btn" onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
-          ☰
-        </button>
-        <nav>
-          <ul>
-            <li><Link className="side-txt" to="/dashboard">🏠 Dashboard</Link></li>
-            <li><Link className="side-txt" to="/dashboard/applyLoan">💰 Apply for Loan</Link></li>
-            <li><Link className="side-txt" to="/dashboard/deposit">🔄 Deposit</Link></li>
-            <li><Link className="side-txt" to="/dashboard/loanPayment">📜 Loan Repayment</Link></li>
-            <li><Link className="side-txt" to="/settings">⚙️ Settings</Link></li>
-            <li><Link className="side-txt" to="/settings">⚙️ Settings</Link></li>
-            <li><Link className="side-txt" to="/settings">⚙️ Settings</Link></li>
-          </ul>
-        </nav>
-      </aside>
-  );
-};
+//   return (
+//       <aside className={`sidebar ${isSidebarOpen ? "open" : "closed"}`}>
+//         <button className="toggle-btn" onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
+//           ☰
+//         </button>
+//         <nav>
+//           <ul>
+//             <li><Link className="side-txt" to="/dashboard">🏠 Dashboard</Link></li>
+//             <li><Link className="side-txt" to="/dashboard/applyLoan">💰 Apply for Loan</Link></li>
+//             <li><Link className="side-txt" to="/dashboard/deposit">🔄 Deposit</Link></li>
+//             <li><Link className="side-txt" to="/dashboard/loanPayment">📜 Loan Repayment</Link></li>
+//             <li><Link className="side-txt" to="/settings">⚙️ Settings</Link></li>
+//             <li><Link className="side-txt" to="/settings">⚙️ Settings</Link></li>
+//             <li><Link className="side-txt" to="/settings">⚙️ Settings</Link></li>
+//           </ul>
+//         </nav>
+//       </aside>
+//   );
+// };
 
-export default Sidebar;
+// export default Sidebar;
