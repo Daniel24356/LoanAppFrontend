8f382fe149206cb2d34010b53ceff8a6b33fbbb7
Daniel24356 <igwedaniel128@gmail.com>
2025-09-18 15:03:11 -0700

    uncommented header.jsx

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,20 +1,20 @@
 
 
-// const Header = () => {
-//     return(
-//         <>
-//          <div className="main-content">
-//          <header className="header">
-//           <h2>Dashboard</h2>
-//           <div className="user-profile">
-//             <span>👤 User Name</span>
-//             <button className="logout-btn">🚪 Logout</button>
-//           </div>
-//         </header>
+const Header = () => {
+    return(
+        <>
+         <div className="main-content">
+         <header className="header">
+          <h2>Dashboard</h2>
+          <div className="user-profile">
+            <span>👤 User Name</span>
+            <button className="logout-btn">🚪 Logout</button>
+          </div>
+        </header>
 
-//       </div>
-//         </>
-//     )
-// }
+      </div>
+        </>
+    )
+}
 
-// export default Header
+export default Header
